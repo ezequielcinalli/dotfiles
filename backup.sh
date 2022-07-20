086252d74c9cf3a82e7a39dc10732b02cabc0757
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-07-20 20:02:17 -0300

    Update files

--- backup.sh
+++ backup.sh
@@ -17,6 +17,7 @@ cp .config/picom.conf $directory.config/
 cp -r .config/nvim/*.vim $directory.config/nvim/
 cp -r .config/i3/ $directory.config/
 cp -r .config/i3blocks/ $directory.config/
+cp -r .config/kitty/ $directory.config/
 cp -r .config/flameshot/ $directory.config/
 cp -r .config/qutebrowser/ $directory.config/
 cp -r .fonts/ $directory/
